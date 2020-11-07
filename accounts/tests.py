8e1ebd5be825cdf0f05c9523e21908747a600d3a
José Andrés Ceballos Vadillo <andres@ceballos.dev>
2020-11-06 21:59:04 -0600

    test of github actions

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+from accounts.models import Page
+
+
+class WebsiteTests(TestCase):
+    def test_page_is_created_successfully(self):
+        page = Page(
+            name='Home',
+            slug='home'
+        )
+        page.save()
