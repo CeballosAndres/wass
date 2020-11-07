90808f240f688d8a06467b9a78b07bd9fdbc6389
José Andrés Ceballos Vadillo <andres@ceballos.dev>
2020-11-07 15:56:06 -0600

    fix env source

--- app/settings.py
+++ app/settings.py
@@ -21,12 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+if os.getenv('GITHUB_WORKFLOW'):
+    SECRET_KEY = getenv('SECRET_KEY)
+    # SECURITY WARNING: don't run with debug turned on in production!
+    DEBUG = TRUE
+else:
+    # SECURITY WARNING: keep the secret key used in production secret!
+    SECRET_KEY = config('SECRET_KEY')
+    # SECURITY WARNING: don't run with debug turned on in production!
+    DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
