ca65fe1ce1a05caa812bfb2e55997cb16528f2a3
AleCentalz <palkya58@gmail.com>
2020-11-08 14:24:42 -0600

    cheking trailing whitespaces

--- accounts/views.py
+++ accounts/views.py
@@ -5,11 +5,14 @@ from django.http import HttpResponse
 def main(request):
     return render(request, 'accounts/main.html')
 
+
 def register(request):
     return render(request, 'accounts/register.html')
 
+
 def login(request):
     return render(request, 'accounts/login.html')
+
 
 def alumno(request):
     return HttpResponse("Hola")
