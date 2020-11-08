8550c220fe8587545e6374b71c8ddbea782c7ae2
AleCentalz <palkya58@gmail.com>
2020-11-07 21:58:19 -0600

    add login page

--- accounts/views.py
+++ accounts/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
+def main(request):
     return render(request, 'accounts/main.html')
 
+def register(request):
+    return render(request, 'accounts/register.html')
+
+def login(request):
+    return render(request, 'accounts/login.html')
 
 def alumno(request):
     return HttpResponse("Hola")
