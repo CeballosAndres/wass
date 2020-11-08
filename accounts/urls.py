dc788bc70997327646373afc112c75d8b2ab2534
AleCentalz <palkya58@gmail.com>
2020-11-08 14:22:34 -0600

    cheking trailing whitespaces

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path('', views.main, name='main'), 
+        path('', views.main, name='main'),
         path('register/', views.register),
         path('login/', views.login),
         ]
