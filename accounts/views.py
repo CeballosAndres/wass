74d2bfc57e54fd363054ce07f31ab81c7a7d721f
AleCentalz <palkya58@gmail.com>
2020-11-07 19:45:24 -0600

    modified views

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'accounts/dashboard.html')
+    return render(request, 'accounts/main.html')
 
 
 def alumno(request):
