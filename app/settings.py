2df5c80a679d8814add9a183b546065340cdfec1
José Andrés Ceballos Vadillo <andres@ceballos.dev>
2020-11-10 00:58:20 -0600

    fix style

--- app/settings.py
+++ app/settings.py
@@ -30,7 +30,7 @@ else:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['www.constru.app','127.0.0.1']
+ALLOWED_HOSTS = ['www.constru.app', '127.0.0.1']
 
 
 # Application definition
