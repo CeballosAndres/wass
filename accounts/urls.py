312fa7aab914d3336001ada1e254a5b06311e1f4
AleCentalz <palkya58@gmail.com>
2020-11-10 14:07:14 -0600

    add pages

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-        path('', views.login),
+        path('', views.ingreso),
         path('registro/', views.registro),
-        path('alumno/', views.mainstudent),
-        ]
+        path('alumno/', views.Pasesorado),
+        path('docente/', views.Pasesor),
+        path('jfe-dpto/', views.PjefeD),
+        path('admi/', views.Padmi),
+        path('agendar/', views.agendar),
+        ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- accounts/views.py
+++ accounts/views.py
@@ -3,12 +3,29 @@ from django.http import HttpResponse
 
 
 def registro(request):
-    return render(request, 'accounts/register.html')
+    return render(request, 'accounts/registro.html')
 
 
-def login(request):
-    return render(request, 'accounts/login.html')
+def ingreso(request):
+    return render(request, 'accounts/ingreso.html')
 
 
-def mainstudent(request):
-    return render(request, 'accounts/main-student.html')
+def Pasesorado(request):
+    return render(request, 'accounts/ppal-asesorado.html')
+
+
+def Pasesor(request):
+    return render(request, 'accounts/ppal-asesor.html')
+
+
+def PjefeD(request):
+    return render(request, 'accounts/ppal-jfedpto.html')
+    
+
+def Padmi(request):
+    return render(request, 'accounts/ppal-adm.html')
+
+
+def agendar(request):
+    return render(request, 'accounts/agendar.html')
+
