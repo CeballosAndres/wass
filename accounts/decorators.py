88ab36dc8aed88344b4282c589424fda245f6194
José Andrés Ceballos Vadillo <andres@ceballos.dev>
2020-11-22 12:31:45 -0600

    add decoratro

--- accounts/decorators.py
+++ accounts/decorators.py
@@ -9,3 +9,12 @@ def unauthenticated_user(view_func):
         else:
             return view_func(request, *args, **kwargs)
     return wrapper_func
+
+
+def allowed_users(allowed_roles=[]):
+    def decorator(view_func):
+        def wrapper_func(request, *args, **kwargs):
+
+            return view_func(request, *args, **kwargs)
+        return wrapper_func
+    return decorator
