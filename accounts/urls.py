44c7599605596d130509f4bc6f7c785f86ab813f
José Andrés Ceballos Vadillo <andres@ceballos.dev>
2020-11-23 22:46:29 -0600

    add user data forms and password change form

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,16 +5,15 @@ from django.conf import settings
 
 urlpatterns = [
         path('', views.principal, name='principal'),
+        path('registro/', views.registro, name='registro'),
         path('ingreso/', views.ingreso, name='ingreso'),
         path('salir/', views.salir, name='salir'),
-        path('registro/', views.registro, name='registro'),
-        path('docente/', views.pAsesor, name='docente'),
-        path('jfe-dpto/', views.pJefeD, name='jfe-dpto'),
+        path('configurar/', views.configurar, name='configurar'),
+
+        path('contrasena/', views.contrasena, name='contrasena'),
         path('admi/', views.pAdmi, name='admi'),
         path('agendar/', views.agendar, name='agendar'),
         path('ver-asesorias/', views.verAsesorias, name='ver-asesorias'),
-        path('config-alumno/', views.cAsesorado, name='config-alumno'),
-        path('config-docente/', views.cAsesor, name='config-docente'),
         path('index/', views.index, name='index'),
         path('reportes/', views.reportes, name='reportes'),
         ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
