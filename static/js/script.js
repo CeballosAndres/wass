9a1a04ae597a8faa1cb4ec8063806dc8f5108802
AleCentalz <palkya58@gmail.com>
2021-01-03 17:33:50 -0600

    add tags titles

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,16 @@
 
- console.log('si funciona la webada jsjs');
+jQuery(document).ready(function ($) {
 
-jQuery(document).ready(function($) {
-
-    $(".clickable-row").click(function() {
+    $(".clickable-row").click(function () {
 
         window.location = $(this).data("href");
 
     });
 
 });
+
+window.setTimeout(function () {
+    $(".alert").fadeTo(500, 0).slideUp(500, function () {
+        $(this).remove();
+    });
+}, 2000);
