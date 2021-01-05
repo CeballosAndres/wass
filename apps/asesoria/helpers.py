ac5d60b45f3709452a480952c6165f3548c25e64
AleCentalz <palkya58@gmail.com>
2021-01-04 20:51:23 -0600

    try to send different emails

--- apps/asesoria/helpers.py
+++ apps/asesoria/helpers.py
@@ -8,15 +8,15 @@ def enviarEmail(accion, asesoria):
         'accion': accion,
         'asesoria': asesoria,
     }
-    #email para el asesorado
+    # email para el asesorado
     msg_plain = render_to_string('asesoria/notificacion_email_asesorado.txt', email_context)
-    #email para el asesor
+    # email para el asesor
     msg_plain_a = render_to_string('asesoria/notificacion_email_asesor.txt', email_context)
-    #msg_html = render_to_string('asesoria/notificacion_email.html', email_context)
+    # msg_html = render_to_string('asesoria/notificacion_email.html', email_context)
     # Send two emails
     datatuple = (
         (f'{accion.capitalize()} de asesoría', msg_plain, 'wass.asesorias@gmail.com', [asesoria.asesorado]),
         (f'{accion.capitalize()} de asesoría', msg_plain_a, 'wass.asesorias@gmail.com', [asesoria.asesor]),
     )
-    
+
     send_mass_mail(datatuple)
