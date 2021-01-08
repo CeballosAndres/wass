7d948ad4c0d033421c87fb862cb4091cde95caa7
José Andrés Ceballos Vadillo <andres@ceballos.dev>
2021-01-08 13:34:10 -0600

    fix cron start

--- app/runner.sh
+++ app/runner.sh
@@ -2,5 +2,6 @@
 python manage.py migrate
 python manage.py collectstatic --noinput
 # python manage.py crontab remove
+/etc/init.d/cron start
 python manage.py crontab add
 gunicorn --bind "0.0.0.0:${PORT}" app.wsgi
